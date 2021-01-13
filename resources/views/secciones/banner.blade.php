5980cefd472eee6e678d7d8f082a6a7d5e7ee8af
juarez1908 <gonza.tauro97@gmail.com>
2021-01-13 14:54:18 -0600

    afinando detalles

--- resources/views/secciones/banner.blade.php
+++ resources/views/secciones/banner.blade.php
@@ -9,20 +9,22 @@
 
             @foreach ($lenguajes as $hijo)
             <div class="col-xs-12 col-sm-6 col-md-4">
-                <div class="widget subscribe no-box" style="background: rgb(249, 231, 159);border-top-left-radius: 30px">
+                <div class="widget subscribe no-box"
+                    style="background: rgb(249, 231, 159);border-top-left-radius: 30px">
                     <br>
                         <h2 class="card-title" style="text-align:center;font-family:Sulphur Point;color:black;">
-                       
-                            {{@$hijo['nombre']}} 
+                            {{@$hijo['nombre']}}
                         </h2>
-                    <p style="text-align:center;font-family:Sulphur Point;color:rgb(242, 243, 244);">
-                        <pre  style="font-family:Sulphur Point; text-align: center;">{{@$hijo['descripcion']}}</pre>
-                    </p>
-                    <center><a href="{{url('/apartado/capitulos/'.$hijo->id)}}" class="btn btn-outline-warning">Ver</a></center>
+                        <p style="text-align:center;font-family:Sulphur Point;color:rgb(242, 243, 244);">
+                            <div style="font-family:Sulphur Point; text-align: center;">{{@$hijo['descripcion']}}</div>
+                        </p>
+                        <center>
+                            <a href="{{url('/apartado/capitulos/'.$hijo->id)}}" class="btn btn-outline-warning">Ver</a>
+                        </center>
                     <br>
                 </div>
             </div>
-           @endforeach
+            @endforeach
         </div>
     </div>
 </div>
